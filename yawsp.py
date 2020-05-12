6e456651f13f11808fe851c7d2a639d7579a5058
cache-sk <cache.sk@gmail.com>
2020-05-12 15:17:11 +0200

    beta

--- yawsp.py
+++ yawsp.py
@@ -32,24 +32,21 @@ def get_url(**kwargs):
     return '{0}?{1}'.format(_url, urlencode(kwargs, 'utf-8'))
 
 def api(fnct, data):
-    print fnct
-    print data
     response = _session.post(API + fnct + "/", data=data)
-    print response.content
     return response
 
 def is_ok(xml):
     status = xml.find('status').text
     return status == 'OK'
 
-def info(message, heading=_addon.getAddonInfo('name'), icon=xbmcgui.NOTIFICATION_INFO, time=3000, sound=False): #NOTIFICATION_WARNING NOTIFICATION_ERROR
+def popinfo(message, heading=_addon.getAddonInfo('name'), icon=xbmcgui.NOTIFICATION_INFO, time=3000, sound=False): #NOTIFICATION_WARNING NOTIFICATION_ERROR
     xbmcgui.Dialog().notification(heading, message, icon, time, sound=sound)
 
 def login():
     username = _addon.getSetting('wsuser')
     password = _addon.getSetting('wspass')
     if username == '' or password == '':
-        info(_addon.getLocalizedString(30101), sound=True)
+        popinfo(_addon.getLocalizedString(30101), sound=True)
         _addon.openSettings()
         return
     response = api('salt', {'username_or_email': username})
@@ -65,10 +62,10 @@ def login():
             _addon.setSetting('token', token)
             return token
         else:
-            info(_addon.getLocalizedString(30102), icon=xbmcgui.NOTIFICATION_ERROR, sound=True)
+            popinfo(_addon.getLocalizedString(30102), icon=xbmcgui.NOTIFICATION_ERROR, sound=True)
             _addon.openSettings()
     else:
-        info(_addon.getLocalizedString(30102), icon=xbmcgui.NOTIFICATION_ERROR, sound=True)
+        popinfo(_addon.getLocalizedString(30102), icon=xbmcgui.NOTIFICATION_ERROR, sound=True)
         _addon.openSettings()
 
 def revalidate():
@@ -83,37 +80,59 @@ def revalidate():
         if is_ok(xml):
             vip = xml.find('vip').text
             if vip != '1':
-                info(_addon.getLocalizedString(30103), icon=xbmcgui.NOTIFICATION_WARNING)
+                popinfo(_addon.getLocalizedString(30103), icon=xbmcgui.NOTIFICATION_WARNING)
             return token
         else:
             if login():
                 return revalidate()
 
-def get_stream_url(ident):
-	token = _addon.getSetting('token')
-	if len(token) == 0:
-		token = login()
-	if token:
-		req = ws_api_request('/file_link/', { 'wst': token, 'ident': ident })
-		link = ET.fromstring(req.text).find('link').text
-		return link
-        
-def labelize(file):
-    size = float(file['size'])
+def todict(xml, skip=[]):
+    result = {}
+    for e in xml:
+        if e.tag not in skip:
+            value = e.text if len(list(e)) == 0 else todict(e,skip)
+            if e.tag in result:
+                if isinstance(result[e.tag], list):
+                    result[e.tag].append(value)
+                else:
+                    result[e.tag] = [result[e.tag],value]
+            else:
+                result[e.tag] = value
+    #result = {e.tag:(e.text if len(list(e)) == 0 else todict(e,skip)) for e in xml if e.tag not in skip}
+    return result
+            
+def sizelize(txtsize, units=['B','KB','MB','GB']):
+    size = float(txtsize)
     if size < 1024:
-        size = str(size) + 'B'
+        size = str(size) + units[0]
     else:
         size = size / 1024
         if size < 1024:
-            size = str(int(round(size))) + 'KB'
+            size = str(int(round(size))) + units[1]
         else:
             size = size / 1024
             if size < 1024:
-                size = str(int(round(size))) + 'MB'
+                size = str(round(size,2)) + units[2]
             else:
                 size = size / 1024
-                size = str(round(size,2)) + 'GB'
+                size = str(round(size,2)) + units[3]
+    return size
+    
+def labelize(file):
+    size = sizelize(file['size'])
     return file['name'] + ' (' + size + ')'
+    
+def tolistitem(file):
+    listitem = xbmcgui.ListItem(label=labelize(file))
+    if 'img' in file:
+        listitem.setArt({'thumb': file['img']})
+    listitem.setInfo('video', {'title': file['name']})
+    listitem.setProperty('IsPlayable', 'true')
+    commands = []
+    commands.append(( _addon.getLocalizedString(30211), 'RunPlugin(' + get_url(action='info',ident=file['ident']) + ')'))
+    #commands.append(( _addon.getLocalizedString(30212), 'RunPlugin(' + get_url(action='download',ident=file['ident']) + ')'))
+    listitem.addContextMenuItems(commands)
+    return listitem
 
 def search(params):
     token = revalidate()
@@ -125,10 +144,9 @@ def queue(params):
     xml = ET.fromstring(response.content)
     if is_ok(xml):
         for file in xml.iter('file'):
-            item = {'ident':file.find('ident').text,'name':file.find('name').text,'size':int(file.find('size').text), 'img':file.find('img').text}
-            listitem = xbmcgui.ListItem(label=labelize(item))
-            listitem.setArt({'thumb': item['img']})
-            xbmcplugin.addDirectoryItem(_handle, get_url(action='file',ident=item['ident']), listitem, True)
+            item = todict(file)
+            listitem = tolistitem(item)
+            xbmcplugin.addDirectoryItem(_handle, get_url(action='play',ident=item['ident'],name=item['name']), listitem, False)
     xbmcplugin.endOfDirectory(_handle)
 
 def history(params):
@@ -138,30 +156,69 @@ def history(params):
     files = []
     if is_ok(xml):
         for file in xml.iter('file'):
-            item = {'ident':file.find('ident').text,'name':file.find('name').text,'size':int(file.find('size').text)}
+            item = todict(file, ['ended_at', 'download_id', 'started_at'])
             if item not in files:
                 files.append(item)
         for file in files:
-            xbmcplugin.addDirectoryItem(_handle, get_url(action='file',ident=file['ident']), xbmcgui.ListItem(label=labelize(file)), True)
+            listitem = tolistitem(file)
+            xbmcplugin.addDirectoryItem(_handle, get_url(action='play',ident=file['ident'],name=item['name']), listitem, False)
     xbmcplugin.endOfDirectory(_handle)
     
 def settings(params):
     _addon.openSettings()
     xbmcplugin.setResolvedUrl(_handle, False, xbmcgui.ListItem())
 
-def file(params):
+def infonize(data,key,process=str,showkey=True,prefix='',suffix='\n'):
+    if key in data:
+        return prefix + (key.capitalize() + ': ' if showkey else '') + process(data[key]) + suffix
+    return ''
+
+def fpsize(fps):
+    x = round(float(fps),3)
+    if int(x) == x:
+       return str(int(x))
+    return str(x)
+
+def info(params):
     token = revalidate()
     response = api('file_info',{'ident':params['ident'],'wst': token})
     xml = ET.fromstring(response.content)
     if is_ok(xml):
-        info = {'name':xml.find('name').text} #TODO
-        listitem = xbmcgui.ListItem(label=_addon.getLocalizedString(30211))
-        #TODO
-        listitem.setInfo('video', {'title': info['name']})
-        listitem.setProperty('IsPlayable', 'true')
-        xbmcplugin.addDirectoryItem(_handle, get_url(action='play',ident=params['ident'],name=info['name']), listitem, False)
-        xbmcplugin.addDirectoryItem(_handle, get_url(action='download',ident=params['ident'],name=info['name']), xbmcgui.ListItem(label=_addon.getLocalizedString(30212)), False)
-    xbmcplugin.endOfDirectory(_handle)
+        info = todict(xml)
+        text = ''
+        text += infonize(info, 'name', lambda x:x)
+        text += infonize(info, 'size', sizelize)
+        text += infonize(info, 'type')
+        text += infonize(info, 'width')
+        text += infonize(info, 'height')
+        text += infonize(info, 'format')
+        text += infonize(info, 'fps', fpsize)
+        text += infonize(info, 'bitrate', lambda x:sizelize(x,['bps','Kbps','Mbps','Gbps']))
+        if 'video' in info and 'stream' in info['video']:
+            streams = info['video']['stream']
+            if isinstance(streams, dict):
+                streams = [streams]
+            for stream in streams:
+                text += 'Video stream: '
+                text += infonize(stream, 'width', showkey=False, suffix='')
+                text += infonize(stream, 'height', showkey=False, prefix='x', suffix='')
+                text += infonize(stream,'format', showkey=False, prefix=', ', suffix='')
+                text += infonize(stream,'fps', fpsize, showkey=False, prefix=', ', suffix='')
+                text += '\n'
+        if 'audio' in info and 'stream' in info['audio']:
+            streams = info['audio']['stream']
+            if isinstance(streams, dict):
+                streams = [streams]
+            for stream in streams:
+                text += 'Audio stream: '
+                text += infonize(stream, 'format', showkey=False, suffix='')
+                text += infonize(stream,'channels', prefix=', ', showkey=False, suffix='')
+                text += infonize(stream,'bitrate', lambda x:sizelize(x,['bps','Kbps','Mbps','Gbps']), prefix=', ', showkey=False, suffix='')
+                text += '\n'
+            
+        xbmcgui.Dialog().textviewer(_addon.getAddonInfo('name'), text)
+    else:
+        popinfo('TODO err', sound=True)
 
 def play(params):
     token = revalidate()
@@ -179,7 +236,7 @@ def play(params):
 
 def download(params):
     token = revalidate()
-    xbmcplugin.setResolvedUrl(_handle, False, xbmcgui.ListItem())
+    #xbmcplugin.setResolvedUrl(_handle, False, xbmcgui.ListItem())
 
 def menu():
     revalidate()
@@ -212,8 +269,8 @@ def router(paramstring):
             history(params)
         elif params['action'] == 'settings':
             settings(params)
-        elif params['action'] == 'file':
-            file(params)
+        elif params['action'] == 'info':
+            info(params)
         elif params['action'] == 'play':
             play(params)
         elif params['action'] == 'download':
